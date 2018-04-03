05fedb3781e76cb9ceb4d30a45cb4934a8a2c1c9
datawrestler <datawrestler@gmail.com>
2018-04-03 11:35:30 -0400

    updated requirements and tag

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def readme():
 
 setup(
     name='model-describer',
-    version='0.1.1',
+    version='0.1.2',
     packages=find_packages("."),
     url='https://github.com/DataScienceSquad/model-describer',
     license='MIT',
